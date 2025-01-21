d2a5d202dd20cac15c3ff6c8f84550f6db4d98b5
Val Andrei Fajardo <andrei@nerdai.io>
2025-01-21 03:22:31 -0500

    fix pre commit

--- src/fed_rag/base/models/base_task_model.py
+++ src/fed_rag/base/models/base_task_model.py
@@ -34,7 +34,7 @@ class BaseTaskModel(BaseModel):
 
     @abstractmethod
     def train(
-        self, train_data: DataT, val_data: DataT, **kwargs
+        self, train_data: DataT, val_data: DataT, **kwargs: Any
     ) -> dict[str, float]:
         """Train the base model using the supplied `trainer_callback`.
 
