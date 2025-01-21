2c4306bd06666b7f67a4a97eaad8791a49ac8063
Val Andrei Fajardo <andrei@nerdai.io>
2025-01-21 10:50:52 -0500

    quick start interface

--- examples/quick_start.py
+++ examples/quick_start.py
@@ -5,9 +5,11 @@ from torch.utils.data import DataLoader
 from fed_rag import create_fl_system_from_trainer
 from fed_rag.decorators import trainer
 
+# define your PyTorch model
 model: torch.nn.Module = ...
 
 
+# define your train loop, wrap it with @trainer decorator
 @trainer
 def train_loop(
     model: torch.nn.Module,
@@ -20,13 +22,20 @@ def train_loop(
     ...
 
 
+# create your fl system
 fl_system = create_fl_system_from_trainer(trainer=train_loop)
 
-# start server
+
+## What can you do with your fl system?
+
+### 1. simulate a run
+sim_results = fl_system.simulate(num_clients=2, strategy="fedavg")
+
+### 2. start server
 fl.server.start_server(
     server=fl_system.server,
     server_address="0.0.0.0:8080",
 )
 
-# start client
+### 3. start client
 fl.client.start_client(server_address="0.0.0.0:8080", client=fl_system.client)
