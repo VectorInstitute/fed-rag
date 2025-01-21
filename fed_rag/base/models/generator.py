8ed8480208272cd793354dd9115ec362a7070eb0
Val Andrei Fajardo <andrei@nerdai.io>
2025-01-20 23:47:17 -0500

    wip

--- fed_rag/base/models/generator.py
+++ fed_rag/base/models/generator.py
@@ -1,1 +1,20 @@
 """Base Generator Model"""
+
+from abc import abstractmethod
+from typing import List
+
+import numpy as np
+import torch.nn as nn
+from pydantic import BaseModel
+
+
+class BaseGeneratorModel(BaseModel):
+    net: nn.Module
+
+    @abstractmethod
+    def get_weights(self) -> List[np.ndarray]:
+        """Get weights of the GeneratorModel's `net`.
+
+        Returns:
+            List[np.array]: List of weight parameters as `numpy.ndarray`s.
+        """
