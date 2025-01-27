a0a55943993d090b454dbba0d914ffa2c009ff2a
Val Andrei Fajardo <andrei@nerdai.io>
2025-01-27 00:50:47 -0500

    add basic unit tests

---
+++ tests/test_decorators.py
@@ -0,0 +1,25 @@
+"""Decorators unit tests"""
+
+from typing import Any
+
+import torch.nn as nn
+
+from fed_rag.decorators import tester, trainer
+
+
+def test_decorated_trainer() -> None:
+    def fn(net: nn.Module) -> Any:
+        pass
+
+    decorated = trainer.pytorch(fn)
+    config = getattr(decorated, "__fl_task_trainer_config")
+    assert len(config) == 0
+
+
+def test_decorated_tester() -> None:
+    def fn(net: nn.Module) -> Any:
+        pass
+
+    decorated = tester.pytorch(fn)
+    config = getattr(decorated, "__fl_task_tester_config")
+    assert len(config) == 0
