1d0e62f57dcee779a749adb2fedb52238244bd9e
Val Andrei Fajardo <andrei@nerdai.io>
2025-01-31 01:43:41 -0500

    re-org

---
+++ src/fed_rag/inspectors/pytorch/tester.py
@@ -0,0 +1,60 @@
+"""PyTorch Tester Inspector"""
+
+import inspect
+from typing import Callable, List
+
+from pydantic import BaseModel
+
+from fed_rag.exceptions import MissingDataParam, MissingNetParam
+
+
+class TesterSignatureSpec(BaseModel):
+    net_parameter: str
+    test_data_param: str
+    extra_test_kwargs: List[str] = []
+
+
+def inspect_tester_signature(fn: Callable) -> TesterSignatureSpec:
+    sig = inspect.signature(fn)
+
+    # inspect fn params
+    extra_tester_kwargs = []
+    net_param = None
+    test_data_param = None
+
+    for name, t in sig.parameters.items():
+        if name in ("self", "cls"):
+            continue
+
+        if type_name := getattr(t.annotation, "__name__", None):
+            if type_name == "Module" and net_param is None:
+                net_param = name
+                continue
+
+            if type_name == "DataLoader" and test_data_param is None:
+                test_data_param = name
+                continue
+
+        extra_tester_kwargs.append(name)
+
+    if net_param is None:
+        msg = (
+            "Inspection failed to find a model param. "
+            "For PyTorch this param must have type `nn.Module`."
+        )
+        raise MissingNetParam(msg)
+
+    if test_data_param is None:
+        msg = (
+            "Inspection failed to find a data param for a test dataset."
+            "For PyTorch this params must be of type `torch.utils.data.DataLoader`"
+        )
+        raise MissingDataParam(msg)
+
+    spec = TesterSignatureSpec(
+        net_parameter=net_param,
+        test_data_param=test_data_param,
+        extra_test_kwargs=extra_tester_kwargs,
+    )
+    print(spec.model_dump(), flush=True)
+    return spec
