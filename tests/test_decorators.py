db4432d2f8967918ed4a919ec6b5593626ab578a
Val Andrei Fajardo <andrei@nerdai.io>
2025-01-31 02:04:26 -0500

    add more unit tests

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -76,6 +76,49 @@ def test_decorated_trainer_fails_due_to_missing_data_loader() -> None:
         federate.trainer.pytorch(fn)
 
 
+def test_decorated_trainer_from_instance_method() -> None:
+    class _TestClass:
+        @federate.trainer.pytorch
+        def fn(
+            self,
+            net: nn.Module,
+            train_loader: DataLoader,
+            val_loader: DataLoader,
+            extra_param_1: int,
+            extra_param_2: float | None,
+        ) -> Any:
+            pass
+
+    obj = _TestClass()
+    config: TrainerSignatureSpec = getattr(obj.fn, "__fl_task_trainer_config")
+    assert config.net_parameter == "net"
+    assert config.train_data_param == "train_loader"
+    assert config.val_data_param == "val_loader"
+    assert config.extra_train_kwargs == ["extra_param_1", "extra_param_2"]
+
+
+def test_decorated_trainer_from_class_method() -> None:
+    class _TestClass:
+        @classmethod
+        @federate.trainer.pytorch
+        def fn(
+            cls,
+            net: nn.Module,
+            train_loader: DataLoader,
+            val_loader: DataLoader,
+            extra_param_1: int,
+            extra_param_2: float | None,
+        ) -> Any:
+            pass
+
+    obj = _TestClass()
+    config: TrainerSignatureSpec = getattr(obj.fn, "__fl_task_trainer_config")
+    assert config.net_parameter == "net"
+    assert config.train_data_param == "train_loader"
+    assert config.val_data_param == "val_loader"
+    assert config.extra_train_kwargs == ["extra_param_1", "extra_param_2"]
+
+
 def test_decorated_tester() -> None:
     def fn(
         mdl: nn.Module,
