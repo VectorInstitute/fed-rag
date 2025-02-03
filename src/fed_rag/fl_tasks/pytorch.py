02d7b3b813dd22b40624e681b9014af7cc39f8e8
Val Andrei Fajardo <andrei@nerdai.io>
2025-02-02 20:53:54 -0500

    wip

--- src/fed_rag/fl_tasks/pytorch.py
+++ src/fed_rag/fl_tasks/pytorch.py
@@ -1,7 +1,8 @@
 """PyTorch FL Task"""
 
-from typing import Any, Callable
+from typing import Any, Callable, OrderedDict
 
+import torch
 import torch.nn as nn
 from flwr.client import NumPyClient
 from pydantic import PrivateAttr
@@ -19,14 +20,58 @@ class PyTorchFLTaskConfig(BaseFLTaskConfig):
     pass
 
 
+class PyTorchFlowerClient(NumPyClient):
+    def __init__(
+        self,
+        net: nn.Module,
+        trainer: Callable,
+        trainer_spec: TrainerSignatureSpec,
+        tester: Callable,
+        tester_spec: TesterSignatureSpec,
+    ) -> None:
+        self.net = net
+        self.trainer = trainer
+        self.trainer_spec = trainer_spec
+        self.tester = tester
+        self.tester_spec = tester_spec
+
+    def get_weights(self) -> Any:
+        return [val.cpu().numpy() for _, val in self.net.state_dict().items()]
+
+    def set_weights(self, parameters) -> None:
+        params_dict = zip(self.net.state_dict().keys(), parameters)
+        state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
+        self.net.load_state_dict(state_dict, strict=True)
+
+    def fit(self, parameters, config) -> Any:
+        self.set_weights(self.net, parameters)
+
+        results = self.trainer(
+            self.net,
+            self.trainloader,
+            self.valloader,
+            self.local_epochs,
+            self.device,
+        )
+        return (
+            self.get_weights(self.net),
+            len(self.trainloader.dataset),
+            results,
+        )
+
+    def evaluate(self, parameters, config) -> Any:
+        self.set_weights(self.net, parameters)
+        loss, accuracy = self.tester(self.net, self.valloader, self.device)
+        return loss, len(self.valloader.dataset), {"accuracy": accuracy}
+
+
 def _build_client(
     net: nn.Module,
     trainer: Callable,
     trainer_spec: TrainerSignatureSpec,
     tester: Callable,
     tester_spec: TesterSignatureSpec,
-) -> NumPyClient:
-    ...
+) -> NumPyClient: ...
 
 
 class PyTorchFLTask(BaseFLTask):
@@ -54,9 +99,7 @@ class PyTorchFLTask(BaseFLTask):
         self._tester_spec = tester_spec
 
         # build flwr NumpyClient
-        self._client = _build_client(
-            net, trainer, trainer_spec, tester, tester_spec
-        )
+        self._client = _build_client(net, trainer, trainer_spec, tester, tester_spec)
 
     @property
     def net(self) -> nn.Module:
@@ -81,9 +124,7 @@ class PyTorchFLTask(BaseFLTask):
         try:
             tester_spec = getattr(trainer, "__fl_task_tester_config")
         except AttributeError:
-            msg = (
-                "Cannot extract `TesterSignatureSpec` from supplied `tester`."
-            )
+            msg = "Cannot extract `TesterSignatureSpec` from supplied `tester`."
             raise MissingTesterSpec(msg)
 
         return cls(
