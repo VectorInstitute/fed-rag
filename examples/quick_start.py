fa2082897b459c1b62132dc32051e23427b2fb56
Val Andrei Fajardo <andrei@nerdai.io>
2025-02-19 16:50:25 -0500

    fully functional quick start?

--- examples/quick_start.py
+++ examples/quick_start.py
@@ -1,20 +1,59 @@
+import signal
 import threading
-from typing import Any
+from typing import Any, Callable
 
+# flwr
 import flwr as fl
+
+# torch
 import torch
 import torch.functional as F
 import torch.nn as nn
+import torchvision.transforms as transforms
 from flwr.client import Client
 from flwr.server import Server
-from flwr.server.server_config import ServerConfig
-from flwr.server.strategy import Strategy
+from flwr_datasets import FederatedDataset
+from flwr_datasets.partitioner import IidPartitioner
 from torch.types import Device
 from torch.utils.data import DataLoader
 
+# fedrag
 from fed_rag.decorators import federate
 from fed_rag.fl_tasks.pytorch import PyTorchFLTask
 from fed_rag.types import TestResult, TrainResult
+
+# partition cifar dataset
+partitioner = IidPartitioner(num_partitions=2)
+fds = FederatedDataset(
+    dataset="uoft-cs/cifar10",
+    partitioners={"train": partitioner},
+)
+
+
+def get_loaders(partition_id: int) -> tuple[DataLoader, DataLoader]:
+    partition = fds.load_partition(partition_id)
+    # Divide data on each node: 80% train, 20% test
+    partition_train_test = partition.train_test_split(test_size=0.2, seed=42)
+    pytorch_transforms = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]
+    )
+
+    def apply_transforms(batch: torch.Tensor) -> torch.Tensor:
+        """Apply transforms to the partition from FederatedDataset."""
+        batch["img"] = [pytorch_transforms(img) for img in batch["img"]]
+        return batch
+
+    partition_train_test = partition_train_test.with_transform(
+        apply_transforms
+    )
+    trainloader = DataLoader(
+        partition_train_test["train"], batch_size=32, shuffle=True
+    )
+    testloader = DataLoader(partition_train_test["test"], batch_size=32)
+    return trainloader, testloader
 
 
 # define your PyTorch model
@@ -53,13 +92,44 @@ def train_loop(
     learning_rate: float | None,
 ) -> TrainResult:
     """My custom train loop."""
-    return TrainResult(loss=0.1)
+
+    model.to(device)  # move model to GPU if available
+    criterion = torch.nn.CrossEntropyLoss().to(device)
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
+    model.train()
+    running_loss = 0.0
+    for _ in range(num_epochs):
+        for batch in train_data:
+            images = batch["img"]
+            labels = batch["label"]
+            optimizer.zero_grad()
+            loss = criterion(model(images.to(device)), labels.to(device))
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+
+    avg_trainloss = running_loss / len(train_data)
+    return TrainResult(loss=avg_trainloss)
 
 
 @federate.tester.pytorch
-def test(m: torch.nn.Module, test_loader: DataLoader) -> TestResult:
+def test(
+    m: torch.nn.Module, test_loader: DataLoader, device: Device
+) -> TestResult:
     """My custom tester."""
-    return TestResult(loss=0.0, metrics={})
+
+    m.to(device)
+    criterion = torch.nn.CrossEntropyLoss()
+    correct, loss = 0, 0.0
+    with torch.no_grad():
+        for batch in test_loader:
+            images = batch["img"].to(device)
+            labels = batch["label"].to(device)
+            outputs = m(images)
+            loss += criterion(outputs, labels).item()
+            correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
+    accuracy = correct / len(test_loader.dataset)
+    return TestResult(loss=loss, metrics={"accuracy": accuracy})
 
 
 # create your fl system
@@ -73,43 +143,28 @@ fl_task = PyTorchFLTask.from_trainer_and_tester(
 ### 1. build a server
 
 # requires the fl strategy and config
-strategy: Strategy = ...
-config: ServerConfig = ...
-
-server = fl_task.server(
-    strategy=strategy,
-    config=config,
-)
-
-if server:
-    fl.server.start_server(
-        server=fl_task.server,
-        server_address="0.0.0.0:8080",
-    )
+server = fl_task.server()
 
 ### 2. build a client trainer
 
 # requires the exact same params as the defined trainer
-train_data = ...
-val_data = ...
-device = ...
-num_epochs = ...
-learning_rate = ...
+clients = {}
+for i in range(2):
+    train_data, val_data = get_loaders(partition_id=i)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    num_epochs = 1
+    learning_rate = 0.1
 
-client = fl_task.client(
-    # train params
-    model=model,
-    train_data=train_data,
-    val_data=val_data,
-    device=device,
-    num_epochs=num_epochs,
-    learning_rate=learning_rate,
-)
-
-if client:
-    fl.client.start_client(
-        server_address="0.0.0.0:8080", client=fl_task.client
+    client = fl_task.client(
+        # train params
+        model=model,
+        train_data=train_data,
+        val_data=val_data,
+        device=device,
+        num_epochs=num_epochs,
+        learning_rate=learning_rate,
     )
+    clients[i] = client
 
 
 def start_local(
@@ -157,10 +212,26 @@ def start_local(
     return thread
 
 
-## BTW, you can still use your training loop as you would in centralized ML
-train_data = ...
-val_data = ...
-num_epochs = ...
-learning_rate = ...
-device = ...
-train_loop(model, train_data, val_data, device, num_epochs, learning_rate)
+def get_signal_handler(event: threading.Event) -> Callable:
+    def signal_handler(signum: int, frame: Any) -> None:
+        nonlocal event
+        event.set()
+
+    return signal_handler
+
+
+## start local
+event = threading.Event()
+server_thread = start_local(
+    event=event,
+    server=server,
+    server_address="0.0.0.0:8080",
+)
+client_0_thread = start_local(
+    event=event, client=clients[0], server_address="0.0.0.0:8080"
+)
+client_1_thread = start_local(
+    event=event, client=clients[1], server_address="0.0.0.0:8080"
+)
+signal.signal(signal.SIGINT, get_signal_handler(event=event))
+signal.pause()
--- src/fed_rag/fl_tasks/pytorch.py
+++ src/fed_rag/fl_tasks/pytorch.py
@@ -92,7 +92,9 @@ class PyTorchFlowerClient(NumPyClient):
         self, parameters: NDArrays, config: dict[str, Scalar]
     ) -> tuple[float, int, dict[str, Scalar]]:
         self.set_weights(parameters)
-        result: TestResult = self.tester(self.net, self.valloader)
+        result: TestResult = self.tester(
+            self.net, self.valloader, **self.extra_test_kwargs
+        )
         return result.loss, len(self.valloader.dataset), result.metrics
 
 
