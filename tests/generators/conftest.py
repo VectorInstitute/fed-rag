3ea162f836f21fad48cabeaae6f1259a8767e142
nerdai <andrei@nerdai.io>
2025-03-04 11:31:32 -0500

    more tests

--- tests/generators/conftest.py
+++ tests/generators/conftest.py
@@ -1,7 +1,53 @@
+from typing import Any
+
 import pytest
 import torch
+from tokenizers import Tokenizer, models
+from transformers import (
+    PretrainedConfig,
+    PreTrainedModel,
+    PreTrainedTokenizer,
+    PreTrainedTokenizerFast,
+)
 
 from fed_rag.base.generator import BaseGenerator
+
+
+class _TestHFConfig(PretrainedConfig):
+    model_type = "testmodel"
+
+    def __init__(self, num_hidden: int = 42, **kwargs: Any):
+        super().__init__(**kwargs)
+        self.num_hidden = num_hidden
+
+
+class _TestHFPretrainedModel(PreTrainedModel):
+    config_class = _TestHFConfig
+
+    def __init__(self, config: _TestHFConfig):
+        super().__init__(config)
+        self.config = config
+        self.model = torch.nn.Linear(3, self.config.num_hidden)
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        return self.model(input)
+
+
+@pytest.fixture
+def dummy_pretrained_model_and_tokenizer() -> (
+    tuple[PreTrainedModel, PreTrainedTokenizer]
+):
+    tokenizer = Tokenizer(models.WordPiece(unk_token="[UNK]"))
+    tokenizer = PreTrainedTokenizerFast(
+        tokenizer_object=tokenizer,
+        unk_token="[UNK]",
+        pad_token="[PAD]",
+        cls_token="[CLS]",
+        sep_token="[SEP]",
+        mask_token="[MASK]",
+    )
+    model = _TestHFPretrainedModel(_TestHFConfig())
+    return model, tokenizer
 
 
 class MockGenerator(BaseGenerator):
--- tests/generators/test_hf_pretrained.py
+++ tests/generators/test_hf_pretrained.py
@@ -1,3 +1,7 @@
+from unittest.mock import MagicMock, patch
+
+from transformers import PreTrainedModel, PreTrainedTokenizer
+
 from fed_rag.base.generator import BaseGenerator
 from fed_rag.generators.hf_pretrained_model import HFPretrainedModelGenerator
 
@@ -17,3 +21,28 @@ def test_hf_pretrained_generator_class_init() -> None:
     assert generator.model_name == "fake_name"
     assert generator._model is None
     assert generator._tokenizer is None
+
+
+@patch.object(HFPretrainedModelGenerator, "_load_model_from_hf")
+def test_hf_pretrained_generator_class_init_with_load(
+    mock_load_from_hf: MagicMock,
+    dummy_pretrained_model_and_tokenizer: tuple[
+        PreTrainedModel, PreTrainedTokenizer
+    ],
+) -> None:
+    # arrange
+    mock_load_from_hf.return_value = dummy_pretrained_model_and_tokenizer
+
+    # act
+    generator = HFPretrainedModelGenerator(
+        model_name="fake_name",
+    )
+    args, kwargs = mock_load_from_hf.call_args
+
+    # assert
+    mock_load_from_hf.assert_called_once()
+    assert generator.model_name == "fake_name"
+    assert generator.model == dummy_pretrained_model_and_tokenizer[0]
+    assert generator.tokenizer == dummy_pretrained_model_and_tokenizer[1]
+    assert args == ()
+    assert kwargs == {}
