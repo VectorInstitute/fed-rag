c982e03c9f61a6c522578c4f2f3e98bba71abef5
Andrei Fajardo <92402603+nerdai@users.noreply.github.com>
2025-03-04 15:00:46 -0500

    `BaseGenerator` and `HFPretrainedModelGenerator` classes (#33)
    * BaseGenerator
    
    * add model property and start of HFPretrainedModelGenerator
    
    * unit test for hf pretrained generator class
    
    * property setter and getters for tokenizers
    
    * more tests
    
    * more coverage
    
    * unit test for using setters
    
    * even more coverage
    
    * generate method

---
+++ src/fed_rag/base/generator.py
@@ -0,0 +1,23 @@
+"""Base Generator"""
+
+from abc import ABC, abstractmethod
+from typing import Any
+
+import torch
+from pydantic import BaseModel, ConfigDict
+
+
+class BaseGenerator(BaseModel, ABC):
+    """Base Generator Class."""
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    @abstractmethod
+    def generate(self, query: str, context: str, **kwargs: Any) -> str:
+        """Generate an output from a given query and context."""
+        ...
+
+    @property
+    @abstractmethod
+    def model(self) -> torch.nn.Module:
+        ...
---
+++ src/fed_rag/generators/hf_pretrained_model.py
@@ -0,0 +1,134 @@
+"""HuggingFace PretrainedModel Generator"""
+
+from typing import Any
+
+import torch
+from pydantic import ConfigDict, Field, PrivateAttr
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    PreTrainedModel,
+    PreTrainedTokenizer,
+)
+from transformers.generation.utils import GenerationConfig
+
+from fed_rag.base.generator import BaseGenerator
+
+DEFAULT_PROMPT_TEMPLATE = """
+You are a helpful assistant. Given the user's question, provide a succinct
+and accurate response. If context is provided, use it in your answer if it helps
+you to create the most accurate response.
+
+<question>
+{question}
+</question>
+
+<context>
+{context}
+</context>
+
+<response>
+
+"""
+
+
+class HFPretrainedModelGenerator(BaseGenerator):
+    model_config = ConfigDict(protected_namespaces=("pydantic_model_",))
+    model_name: str = Field(
+        description="Name of HuggingFace model. Used for loading the model from HF hub or local."
+    )
+    generation_config: GenerationConfig = Field(
+        description="The generation config used for generating with the PreTrainedModel."
+    )
+    load_model_kwargs: dict | None = Field(
+        description="Optional kwargs dict for loading models from HF. Defaults to None.",
+        default=None,
+    )
+    prompt_template: str = Field(description="Prompt template for RAG.")
+    _model: PreTrainedModel | None = PrivateAttr(default=None)
+    _tokenizer: PreTrainedTokenizer | None = PrivateAttr(default=None)
+
+    def __init__(
+        self,
+        model_name: str,
+        generation_config: GenerationConfig | None = None,
+        prompt_template: str | None = None,
+        load_model_kwargs: dict | None = None,
+        load_model_at_init: bool = True,
+    ):
+        generation_config = (
+            generation_config if generation_config else GenerationConfig()
+        )
+        prompt_template = (
+            prompt_template if prompt_template else DEFAULT_PROMPT_TEMPLATE
+        )
+        super().__init__(
+            model_name=model_name,
+            generation_config=generation_config,
+            prompt_template=prompt_template,
+            load_model_kwargs=load_model_kwargs,
+        )
+        if load_model_at_init:
+            load_model_kwargs = load_model_kwargs if load_model_kwargs else {}
+            self._model, self._tokenizer = self._load_model_from_hf(
+                **load_model_kwargs
+            )
+
+    def _load_model_from_hf(
+        self,
+    ) -> tuple[PreTrainedModel, PreTrainedTokenizer]:
+        load_kwargs = self.load_model_kwargs if self.load_model_kwargs else {}
+        model = AutoModelForCausalLM.from_pretrained(
+            self.model_name, **load_kwargs
+        )
+        tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+        return model, tokenizer
+
+    @property
+    def model(self) -> PreTrainedModel:
+        if self._model is None:
+            # load HF Pretrained Model
+            model, _ = self._load_model_from_hf()
+            self._model = model
+        return self._model
+
+    @model.setter
+    def model(self, value: PreTrainedModel) -> None:
+        self._model = value
+
+    @property
+    def tokenizer(self) -> PreTrainedTokenizer:
+        if self._tokenizer is None:
+            # load HF Pretrained Model
+            _, tokenizer = self._load_model_from_hf()
+            self._tokenizer = tokenizer
+        return self._tokenizer
+
+    @tokenizer.setter
+    def tokenizer(self, value: PreTrainedTokenizer) -> None:
+        self._tokenizer = value
+
+    # generate
+    def generate(self, query: str, context: str, **kwargs: Any) -> str:
+        formatted_query = self.prompt_template.format(
+            question=query, context=context
+        )
+
+        # encode query
+        tokenizer_result = self.tokenizer(formatted_query, return_tensors="pt")
+        inputs: torch.Tensor = tokenizer_result.input_ids
+        inputs = inputs.to(self.model.device)
+
+        # generate
+
+        generated_ids = self.model.generate(
+            inputs=inputs,
+            generation_config=self.generation_config,
+            tokenizer=self._tokenizer,
+        )
+
+        # decode tokens
+        outputs: list[str] = self.tokenizer.batch_decode(
+            generated_ids, skip_special_tokens=True
+        )
+        return outputs[0]
