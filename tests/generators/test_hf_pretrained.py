1c09b9af737d822769473a41de368c91a4704cb1
nerdai <andrei@nerdai.io>
2025-03-04 11:38:02 -0500

    more coverage

--- tests/generators/test_hf_pretrained.py
+++ tests/generators/test_hf_pretrained.py
@@ -13,7 +13,13 @@ def test_hf_pretrained_generator_class() -> None:
     assert BaseGenerator.__name__ in names_of_base_classes
 
 
-def test_hf_pretrained_generator_class_init() -> None:
+@patch.object(HFPretrainedModelGenerator, "_load_model_from_hf")
+def test_hf_pretrained_generator_class_init_delayed_load(
+    mock_load_from_hf: MagicMock,
+    dummy_pretrained_model_and_tokenizer: tuple[
+        PreTrainedModel, PreTrainedTokenizer
+    ],
+) -> None:
     generator = HFPretrainedModelGenerator(
         model_name="fake_name", load_model_at_init=False
     )
@@ -22,9 +28,21 @@ def test_hf_pretrained_generator_class_init() -> None:
     assert generator._model is None
     assert generator._tokenizer is None
 
+    # load model
+    mock_load_from_hf.return_value = dummy_pretrained_model_and_tokenizer
+
+    generator._load_model_from_hf()
+    args, kwargs = mock_load_from_hf.call_args
+
+    mock_load_from_hf.assert_called_once()
+    assert generator.model == dummy_pretrained_model_and_tokenizer[0]
+    assert generator.tokenizer == dummy_pretrained_model_and_tokenizer[1]
+    assert args == ()
+    assert kwargs == {}
+
 
 @patch.object(HFPretrainedModelGenerator, "_load_model_from_hf")
-def test_hf_pretrained_generator_class_init_with_load(
+def test_hf_pretrained_generator_class_init(
     mock_load_from_hf: MagicMock,
     dummy_pretrained_model_and_tokenizer: tuple[
         PreTrainedModel, PreTrainedTokenizer
