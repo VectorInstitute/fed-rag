b81bd196658809452bc04327d3f4c43d9d217a8e
nerdai <andrei@nerdai.io>
2025-03-08 11:10:29 -0500

    add validation of KnowledgeNode

--- src/fed_rag/types/knowledge_node.py
+++ src/fed_rag/types/knowledge_node.py
@@ -3,7 +3,7 @@
 import uuid
 from enum import Enum
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ValidationInfo, field_validator
 
 
 class NodeType(str, Enum):
@@ -18,14 +18,14 @@ class KnowledgeNode(BaseModel):
         description="Encoded representation of node."
     )
     node_type: NodeType = Field(description="Type of node.")
-    metadata: dict = Field(
-        description="Metadata for node.", default_factory=dict
-    )
     text_content: str | None = Field(
         description="Text content. Used for TEXT and potentially MULTIMODAL node types."
     )
     image_content: bytes | None = Field(
         description="Image content as binary data (decoded from base64)"
+    )
+    metadata: dict = Field(
+        description="Metadata for node.", default_factory=dict
     )
 
     def get_content(self) -> str | bytes | dict | None:
@@ -36,3 +36,44 @@ class KnowledgeNode(BaseModel):
             return self.image_content
         elif self.node_type == NodeType.MULTIMODAL:
             return {"text": self.text_content, "image": self.image_content}
+
+    # validators
+    @field_validator("text_content", mode="after")
+    @classmethod
+    def validate_text_content(
+        cls, value: str | None, info: ValidationInfo
+    ) -> str | None:
+        node_type: NodeType = info.data.get("node_type")
+        if node_type == NodeType.TEXT:
+            if value is None:
+                raise ValueError(
+                    "NodeType == 'text', but text_content is None."
+                )
+
+        if node_type == NodeType.MULTIMODAL:
+            if value is None:
+                raise ValueError(
+                    "NodeType == 'multimodal', but text_content is None."
+                )
+
+        return value
+
+    @field_validator("image_content", mode="after")
+    @classmethod
+    def validate_image_content(
+        cls, value: str | None, info: ValidationInfo
+    ) -> str | None:
+        node_type: NodeType = info.data.get("node_type")
+        if node_type == NodeType.IMAGE:
+            if value is None:
+                raise ValueError(
+                    "NodeType == 'image', but image_content is None."
+                )
+
+        if node_type == NodeType.MULTIMODAL:
+            if value is None:
+                raise ValueError(
+                    "NodeType == 'multimodal', but image_content is None."
+                )
+
+        return value
