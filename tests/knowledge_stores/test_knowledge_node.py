f87b3481dd5a578b7df92bc48f505521e7d4bc60
nerdai <andrei@nerdai.io>
2025-03-08 11:43:38 -0500

    unit tests for image type of knowledge node

--- tests/knowledge_stores/test_knowledge_node.py
+++ tests/knowledge_stores/test_knowledge_node.py
@@ -25,3 +25,28 @@ def test_text_knowledge_node_init_raises_validation_error() -> None:
         KnowledgeNode(
             node_id="mock_id", embedding=[0.1, 0.2, 0.3], node_type="text"
         )
+
+
+@patch("fed_rag.types.knowledge_node.uuid")
+def test_image_knowledge_node_init(mock_uuid: MagicMock) -> None:
+    mock_uuid.uuid4.return_value = "mock_id"
+    node = KnowledgeNode(
+        embedding=[0.1, 0.2, 0.3],
+        node_type="image",
+        image_content=b"mock_base64_str",
+    )
+
+    assert node.node_id == "mock_id"
+    assert node.embedding == [0.1, 0.2, 0.3]
+    assert node.node_type == "image"
+    assert isinstance(node.image_content, bytes)
+    assert node.image_content == b"mock_base64_str"
+
+
+def test_image_knowledge_node_init_raises_validation_error() -> None:
+    with pytest.raises(
+        ValueError, match="NodeType == 'image', but image_content is None."
+    ):
+        KnowledgeNode(
+            node_id="mock_id", embedding=[0.1, 0.2, 0.3], node_type="image"
+        )
