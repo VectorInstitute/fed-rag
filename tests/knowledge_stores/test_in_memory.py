0da3ce599157966f09294c9e7d3cb4f5896c0857
nerdai <andrei@nerdai.io>
2025-03-08 20:20:05 -0500

    define nodes fixture

--- tests/knowledge_stores/test_in_memory.py
+++ tests/knowledge_stores/test_in_memory.py
@@ -1,6 +1,23 @@
+import pytest
+
 from fed_rag.base.knowledge_store import BaseKnowledgeStore
 from fed_rag.knowledge_stores.in_memory import InMemoryKnowledgeStore
 from fed_rag.types.knowledge_node import KnowledgeNode
+
+
+@pytest.fixture
+def text_nodes() -> list[KnowledgeNode]:
+    return [
+        KnowledgeNode(
+            embedding=[0.1], node_type="text", text_content="node 1"
+        ),
+        KnowledgeNode(
+            embedding=[0.2], node_type="text", text_content="node 2"
+        ),
+        KnowledgeNode(
+            embedding=[0.3], node_type="text", text_content="node 3"
+        ),
+    ]
 
 
 def test_in_memory_knowledge_store_class() -> None:
@@ -16,19 +33,8 @@ def test_in_memory_knowledge_store_init() -> None:
     assert knowledge_store.count == 0
 
 
-def test_from_nodes() -> None:
-    nodes = [
-        KnowledgeNode(
-            embedding=[0.1], node_type="text", text_content="node 1"
-        ),
-        KnowledgeNode(
-            embedding=[0.2], node_type="text", text_content="node 2"
-        ),
-        KnowledgeNode(
-            embedding=[0.3], node_type="text", text_content="node 3"
-        ),
-    ]
-    knowledge_store = InMemoryKnowledgeStore.from_nodes(nodes=nodes)
+def test_from_nodes(text_nodes: list[KnowledgeNode]) -> None:
+    knowledge_store = InMemoryKnowledgeStore.from_nodes(nodes=text_nodes)
 
     assert knowledge_store.count == 3
-    assert all(n.node_id in knowledge_store._data for n in nodes)
+    assert all(n.node_id in knowledge_store._data for n in text_nodes)
