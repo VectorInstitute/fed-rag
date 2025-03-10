8b655ef96e0fe54d904767961124579f5471cb9f
nerdai <andrei@nerdai.io>
2025-03-10 16:53:04 -0400

    update to use HFSentenceTransformerRetriever

--- examples/ra-dit/dragon.py
+++ examples/ra-dit/dragon.py
@@ -1,64 +1,13 @@
 """Dragon Retriever."""
 
-from typing import Optional
-
-from pydantic import BaseModel, PrivateAttr
-from sentence_transformers import SentenceTransformer
-from torch import Tensor
-
-
-class DragonRetriever(BaseModel):
-    _encoder: Optional[SentenceTransformer] = PrivateAttr(default=None)
-    _query_encoder: Optional[SentenceTransformer] = PrivateAttr(default=None)
-    _context_encoder: Optional[SentenceTransformer] = PrivateAttr(default=None)
-
-    def __init__(
-        self,
-        encoder: Optional[SentenceTransformer] = None,
-        query_encoder: Optional[SentenceTransformer] = None,
-        context_encoder: Optional[SentenceTransformer] = None,
-    ) -> None:
-        """Init method."""
-
-        if encoder is not None:
-            if query_encoder is not None:
-                raise ValueError(
-                    "If `encoder` is supplied, then cannot provide `query_encoder` as well."
-                )
-            if context_encoder is not None:
-                raise ValueError(
-                    "If `encoder` is supplied, then cannot provide `query_encoder` as well."
-                )
-        else:
-            if query_encoder is None or context_encoder is None:
-                raise ValueError(
-                    "If `encoder` is None, then must supply both a `query_encoder` and a `context_encoder`."
-                )
-
-        super().__init__()
-        self._encoder = encoder
-        self._query_encoder = query_encoder
-        self._context_encoder = context_encoder
-
-    def encode_query(self, query: str | list[str]) -> Tensor:
-        encoder: SentenceTransformer = (
-            self._query_encoder if self._query_encoder else self._encoder
-        )
-        return encoder.encode(query)
-
-    def encode_context(self, context: str | list[str]) -> Tensor:
-        encoder: SentenceTransformer = (
-            self._context_encoder if self._context_encoder else self._encoder
-        )
-        return encoder.encode(context)
-
+from fed_rag.retrievers.hf_sentence_transformer import (
+    HFSentenceTransformerRetriever,
+)
 
 if __name__ == "__main__":
-    dragon_retriever = DragonRetriever(
-        query_encoder=SentenceTransformer("nthakur/dragon-plus-query-encoder"),
-        context_encoder=SentenceTransformer(
-            "nthakur/dragon-plus-context-encoder"
-        ),
+    dragon_retriever = HFSentenceTransformerRetriever(
+        query_model_name="nthakur/dragon-plus-query-encoder",
+        context_model_name="nthakur/dragon-plus-context-encoder",
     )
 
     query = "Where was Marie Curie born?"
