e3d22cd0997217294023536bb59aa666198d22fa
nerdai <andrei@nerdai.io>
2025-03-11 14:16:33 -0400

    add script to build knowledge_store

--- examples/ra-dit/knowledge_store.py
+++ examples/ra-dit/knowledge_store.py
@@ -1,9 +1,15 @@
 """Knowledge Store."""
 
+import json
+from pathlib import Path
+
 from fed_rag.knowledge_stores.in_memory import InMemoryKnowledgeStore
 from fed_rag.retrievers.hf_sentence_transformer import (
     HFSentenceTransformerRetriever,
 )
+from fed_rag.types.knowledge_node import KnowledgeNode, NodeType
+
+DATA_PATH = Path(__file__).parents[0].absolute() / "data"
 
 knowledge_store = InMemoryKnowledgeStore()
 dragon_retriever = HFSentenceTransformerRetriever(
@@ -11,6 +17,26 @@ dragon_retriever = HFSentenceTransformerRetriever(
     context_model_name="nthakur/dragon-plus-context-encoder",
 )
 
-# load chunks from atlas corpus into knowledge store
+# load chunks from atlas corpus
+filename = "mock_data.jsonl"
+with open(DATA_PATH / filename) as f:
+    chunks = [json.loads(line) for line in f]
+
+# create knowledge nodes
+nodes = []
+for c in chunks:
+    node = KnowledgeNode(
+        embedding=dragon_retriever.encode_context(c["text"]).tolist(),
+        node_type=NodeType.TEXT,
+        text_content=c["text"],
+        metadata={"title": c["title"], "id": c["id"]},
+    )
+    nodes.append(node)
+
+# load into knowledge_store
+knowledge_store.load_nodes(nodes=nodes)
 
 # persist to disk
+
+if __name__ == "__main__":
+    print(knowledge_store.count)
