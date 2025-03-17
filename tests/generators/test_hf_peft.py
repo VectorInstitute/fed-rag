7f31b6d484b861f6e3068fd120876362473b1f70
nerdai <andrei@nerdai.io>
2025-03-16 23:43:29 -0400

    unit test for init with delayed load

---
+++ tests/generators/test_hf_peft.py
@@ -0,0 +1,41 @@
+from unittest.mock import MagicMock, patch
+
+from peft import PeftModel
+from transformers import PreTrainedTokenizer
+
+from fed_rag.base.generator import BaseGenerator
+from fed_rag.generators.hf_peft_model import HFPeftModelGenerator
+
+
+def test_hf_pretrained_generator_class() -> None:
+    names_of_base_classes = [b.__name__ for b in HFPeftModelGenerator.__mro__]
+    assert BaseGenerator.__name__ in names_of_base_classes
+
+
+@patch.object(HFPeftModelGenerator, "_load_model_from_hf")
+def test_hf_pretrained_generator_class_init_delayed_load(
+    mock_load_from_hf: MagicMock,
+    dummy_peft_model_and_tokenizer: tuple[PeftModel, PreTrainedTokenizer],
+) -> None:
+    generator = HFPeftModelGenerator(
+        model_name="fake_name",
+        base_model_name="fake_base_name",
+        load_model_at_init=False,
+    )
+
+    assert generator.model_name == "fake_name"
+    assert generator.base_model_name == "fake_base_name"
+    assert generator._model is None
+    assert generator._tokenizer is None
+
+    # load model
+    mock_load_from_hf.return_value = dummy_peft_model_and_tokenizer
+
+    generator._load_model_from_hf()
+    args, kwargs = mock_load_from_hf.call_args
+
+    mock_load_from_hf.assert_called_once()
+    assert generator.model == dummy_peft_model_and_tokenizer[0]
+    assert generator.tokenizer == dummy_peft_model_and_tokenizer[1]
+    assert args == ()
+    assert kwargs == {}
