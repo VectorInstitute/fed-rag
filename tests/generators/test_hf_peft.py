7ee189952d15f824a6847e3d30a5bed44cf94654
nerdai <andrei@nerdai.io>
2025-03-16 23:45:28 -0400

    add unit test for load at init

--- tests/generators/test_hf_peft.py
+++ tests/generators/test_hf_peft.py
@@ -39,3 +39,27 @@ def test_hf_pretrained_generator_class_init_delayed_load(
     assert generator.tokenizer == dummy_peft_model_and_tokenizer[1]
     assert args == ()
     assert kwargs == {}
+
+
+@patch.object(HFPeftModelGenerator, "_load_model_from_hf")
+def test_hf_pretrained_generator_class_init(
+    mock_load_from_hf: MagicMock,
+    dummy_peft_model_and_tokenizer: tuple[PeftModel, PreTrainedTokenizer],
+) -> None:
+    # arrange
+    mock_load_from_hf.return_value = dummy_peft_model_and_tokenizer
+
+    # act
+    generator = HFPeftModelGenerator(
+        model_name="fake_name", base_model_name="fake_base_name"
+    )
+    args, kwargs = mock_load_from_hf.call_args
+
+    # assert
+    mock_load_from_hf.assert_called_once()
+    assert generator.model_name == "fake_name"
+    assert generator.base_model_name == "fake_base_name"
+    assert generator.model == dummy_peft_model_and_tokenizer[0]
+    assert generator.tokenizer == dummy_peft_model_and_tokenizer[1]
+    assert args == ()
+    assert kwargs == {}
