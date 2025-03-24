22b4b3db27a6b41abeb5162eba92eb61a606b440
nerdai <andrei@nerdai.io>
2025-03-24 02:31:53 -0400

    fix tests

--- src/fed_rag/utils/data/finetuning_datasets/__init__.py
+++ src/fed_rag/utils/data/finetuning_datasets/__init__.py
@@ -1,8 +1,5 @@
-from .huggingface import HuggingfaceRAGFinetuningDataset
 from .pytorch import PyTorchRAGFinetuningDataset
 
 __all__ = [
     "PyTorchRAGFinetuningDataset",
-    # requires huggingface extra
-    "HuggingfaceRAGFinetuningDataset",
 ]
--- src/fed_rag/utils/data/finetuning_datasets/huggingface.py
+++ src/fed_rag/utils/data/finetuning_datasets/huggingface.py
@@ -6,10 +6,12 @@ from typing_extensions import Self
 # check if huggingface extra was installed
 try:
     from datasets import Dataset
-
-    _has_huggingface = True
 except ModuleNotFoundError:
-    _has_huggingface = False
+    msg = (
+        "`HuggingfaceRAGFinetuningDataset` requires the `huggingface` extra to be installed. "
+        "To fix please run `pip install fed-rag[huggingface]`."
+    )
+    raise ValueError(msg)
 
 
 class HuggingfaceRAGFinetuningDataset(Dataset):
@@ -19,12 +21,6 @@ class HuggingfaceRAGFinetuningDataset(Dataset):
     def from_inputs(
         cls, input_ids: torch.Tensor, target_ids: torch.Tensor
     ) -> Self:
-        if not _has_huggingface:
-            msg = (
-                f"`{cls.__name__}` requires the `huggingface` extra to be installed. "
-                "To fix please run `pip install fed-rag[huggingface]`."
-            )
-            raise ValueError(msg)
         return cls.from_dict(  # type: ignore[no-any-return]
             {"input_ids": input_ids, "target_ids": target_ids}
         )
