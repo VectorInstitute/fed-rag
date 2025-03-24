f0b1cfd459befcade1c402b6c8c8fa6b0e15d883
nerdai <andrei@nerdai.io>
2025-03-23 23:47:09 -0400

    add data module

---
+++ src/fed_rag/utils/data/finetuning_datasets/huggingface.py
@@ -0,0 +1,26 @@
+"""HuggingFace RAG Finetuning Dataset"""
+
+import torch
+from typing_extensions import Self
+
+# check if huggingface extra was installed
+try:
+    from datasets import Dataset
+except ModuleNotFoundError:
+    msg = (
+        "HuggingFace finetuning datasets requires the `huggingface` extra to be installed. "
+        "To fix please run `pip install fed-rag[huggingface]`."
+    )
+    raise ValueError(msg)
+
+
+class HuggingfaceRAGFinetuningDataset(Dataset):
+    """Thin wrapper over ~datasets.Dataset."""
+
+    @classmethod
+    def from_inputs(
+        cls, input_ids: torch.Tensor, target_ids: torch.Tensor
+    ) -> Self:
+        return cls.from_dict(  # type: ignore[no-any-return]
+            {"input_ids": input_ids, "target_ids": target_ids}
+        )
---
+++ src/fed_rag/utils/data/finetuning_datasets/pytorch.py
@@ -0,0 +1,18 @@
+"""PyTorch RAG Finetuning Dataset"""
+
+from typing import Any
+
+import torch
+from torch.utils.data import Dataset
+
+
+class PyTorchRAGFinetuningDataset(Dataset):
+    def __init__(self, input_ids: torch.Tensor, target_ids: torch.Tensor):
+        self.input_ids = input_ids
+        self.target_ids = target_ids
+
+    def __len__(self) -> int:
+        return len(self.input_ids)
+
+    def __getitem__(self, idx: int) -> Any:
+        return self.input_ids[idx], self.target_ids[idx]
