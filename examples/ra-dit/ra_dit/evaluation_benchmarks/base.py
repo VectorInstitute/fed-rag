91a724662f175df553286096befa9ac136b15ff5
Andrei Fajardo <92402603+nerdai@users.noreply.github.com>
2025-04-11 14:04:56 -0400

    Example: `RA-DIT` centralized training pipeline (#154)
    * centralized
    
    * introduce new BaseBenchmark
    
    * add threading for evaluation
    
    * MMLUBenchmark
    
    * MMLUBenchmark
    
    * wip
    
    * wip
    
    * merge lora weights when inferen

---
+++ examples/ra-dit/ra_dit/evaluation_benchmarks/base.py
@@ -0,0 +1,130 @@
+"""Base Benchmark."""
+
+import logging
+from abc import ABC, abstractmethod
+from concurrent.futures import Future, ThreadPoolExecutor
+from threading import Lock
+from typing import Any
+
+import pandas as pd
+from pydantic import BaseModel, ConfigDict
+from typing_extensions import Self
+
+from fed_rag.types.rag_system import RAGSystem
+
+
+class BenchmarkResult(BaseModel):
+    score: float
+
+
+class ExamplePred(BaseModel):
+    pred: Any
+
+    def __str__(self) -> str:
+        return str(self.pred)
+
+
+class ScoredExamplePred(ExamplePred):
+    score: float
+
+    @classmethod
+    def from_example_pred(cls, pred: ExamplePred, score: float) -> Self:
+        return cls(pred=pred.pred, score=score)
+
+
+class BaseBenchmark(BaseModel, ABC):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    examples: pd.DataFrame
+    generate_prompt_template: str | None
+    logger: logging.Logger = logging.getLogger("ra_dit.benchmark")
+
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        """Name of benchmark."""
+
+    @abstractmethod
+    def _predict_example(
+        self, example: pd.Series, rag_system: RAGSystem
+    ) -> ExamplePred:
+        """Logic for predicting an example with a given 'rag_system'."""
+
+    @abstractmethod
+    def _evaluate_prediction(
+        self, example: pd.Series, pred: ExamplePred
+    ) -> ScoredExamplePred:
+        """Evaluate a prediction."""
+
+    @abstractmethod
+    def _aggregate_example_scores(
+        self, scored_examples: list[ScoredExamplePred]
+    ) -> float:
+        """Custom aggregated example scores.
+
+        Subclasses implement this method.
+        """
+
+    def aggregate_example_scores(
+        self, scored_examples: list[ScoredExamplePred]
+    ) -> float:
+        """Aggregated example scores"""
+        return self._aggregate_example_scores(scored_examples=scored_examples)
+
+    def _update_prompt_template(self, rag_system: RAGSystem) -> None:
+        if self.generate_prompt_template and hasattr(
+            rag_system.generator, "prompt_template"
+        ):
+            rag_system.generator.prompt_template = (
+                self.generate_prompt_template
+            )
+
+    def run(
+        self, rag_system: RAGSystem, num_threads: int = 1
+    ) -> BenchmarkResult:
+        """Run the benchmark with the given rag_system."""
+        self.logger.info(
+            f"Running benchmark {self.name} with num_threads: {num_threads}"
+        )
+
+        self._update_prompt_template(rag_system=rag_system)
+        tasks_total = len(self.examples)
+        tasks_completed = 0
+        lock = Lock()
+
+        def process_example(example: pd.Series) -> ScoredExamplePred:
+            pred = self._predict_example(
+                example=example, rag_system=rag_system
+            )
+            return self._evaluate_prediction(example=example, pred=pred)
+
+        def progress_indicator(future: Future) -> None:
+            nonlocal tasks_completed
+            with lock:
+                tasks_completed += 1
+                self.logger.debug(
+                    f"{tasks_completed}/{tasks_total} completed, {tasks_total-tasks_completed} remain."
+                )
+
+        if num_threads > 1:
+            with ThreadPoolExecutor(max_workers=num_threads) as executor:
+                futures = [
+                    executor.submit(process_example, e)
+                    for _, e in self.examples.iterrows()
+                ]
+                for future in futures:
+                    future.add_done_callback(progress_indicator)
+                scored_examples = [f.result() for f in futures]
+        else:
+            log_interval = max(1, tasks_total // 10)
+            scored_examples = []
+            for ix, example in self.examples.iterrows():
+                scored_examples.append(process_example(example=example))
+                if (ix + 1) % log_interval == 0:
+                    self.logger.debug(
+                        f"Processed {ix + 1} / {tasks_total} examples"
+                    )
+
+        agg_score = self.aggregate_example_scores(
+            scored_examples=scored_examples
+        )
+        return BenchmarkResult(score=agg_score)
