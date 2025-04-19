45f290b58484fbc1025d9af15f19adfb311ba970
Andrei Fajardo <92402603+nerdai@users.noreply.github.com>
2025-04-18 23:33:18 -0400

    [Feature] Add LM Supervised Retriever Loss `LSRLoss` (#182)
    * LSR basic implementation
    
    * start unit tests
    
    * test forward
    
    * test forward
    
    * pragma no cover

---
+++ src/fed_rag/loss/pytorch/lsr.py
@@ -0,0 +1,63 @@
+"""LM-Supervised Retriever Loss."""
+
+from enum import Enum
+
+import torch
+import torch.functional as F
+import torch.nn as nn
+from typing_extensions import assert_never
+
+from fed_rag.exceptions.loss import InvalidReductionParam
+
+
+class ReductionMode(str, Enum):
+    MEAN = "mean"
+    SUM = "sum"
+
+    @classmethod
+    def members_list(cls) -> list[str]:
+        return [member for member in cls]
+
+
+class LSRLoss(nn.Module):
+    """PyTorch implementation of the LM-Supervised Retriever Loss.
+
+    Given input context x and ground truth continuation y, computes KL divergence
+    between retrieval likelihood P_R(d|x) and language model likelihood Q_LM(d|x,y),
+    where d is the retrieved document.
+
+    Source: Shi, Weijia, et al. "Replug: Retrieval-augmented black-box language models."
+        arXiv preprint arXiv:2301.12652 (2023).
+    Arxiv: https://arxiv.org/pdf/2301.12652
+    """
+
+    def __init__(self, reduction: ReductionMode = ReductionMode.MEAN):
+        # This line is critical - it initializes all the Module machinery
+        super(LSRLoss, self).__init__()
+
+        if reduction not in ReductionMode.members_list():
+            msg = (
+                f"Invalid reduction {reduction}. "
+                f"Valid reductions are: {', '.join(ReductionMode.members_list())}"
+            )
+            raise InvalidReductionParam(msg)
+
+        self.reduction = reduction
+
+    def forward(
+        self, retrieval_logits: torch.Tensor, lm_logits: torch.Tensor
+    ) -> torch.Tensor:
+        retrieval_probs = F.softmax(retrieval_logits, dim=1)
+        lm_probs = F.softmax(lm_logits, dim=1)
+
+        kl_div = F.kl_div(retrieval_probs, lm_probs, reduction="none").sum(
+            dim=-1
+        )
+
+        match self.reduction:
+            case ReductionMode.MEAN:
+                return kl_div.mean()
+            case ReductionMode.SUM:
+                return kl_div.sum()
+            case _:  # pragma: no cover
+                assert_never(self.reduction)  # pragma: no cover
---
+++ tests/loss/pytorch/test_lsr.py
@@ -0,0 +1,48 @@
+from unittest.mock import MagicMock, _Call, patch
+
+import pytest
+import torch
+
+from fed_rag.exceptions.loss import InvalidReductionParam
+from fed_rag.loss.pytorch.lsr import LSRLoss, ReductionMode
+
+
+def test_lsr_loss_init() -> None:
+    loss = LSRLoss(reduction="sum")
+
+    assert loss.reduction == ReductionMode.SUM
+
+
+def test_invalid_reduction_raises_error() -> None:
+    with pytest.raises(InvalidReductionParam):
+        LSRLoss(reduction="invalid_reduction")
+
+
+@pytest.mark.parametrize(
+    ("reduction", "expected"), [("mean", 10.5), ("sum", 21)]
+)
+@patch("fed_rag.loss.pytorch.lsr.F")
+def test_lsr_forward(
+    mock_torch_functional: MagicMock, reduction: str, expected: float
+) -> None:
+    # arrange mocks
+    mock_torch_functional.softmax.side_effect = iter(
+        [torch.Tensor([1, 2, 3]), torch.Tensor([4, 5, 6])]
+    )
+    mock_torch_functional.kl_div.return_value = torch.Tensor(
+        [[1, 2, 3], [4, 5, 6]]
+    )
+
+    loss = LSRLoss(reduction=reduction)
+    retrieval_logits = torch.zeros(3)
+    lm_logits = torch.zeros(3)
+    out = loss(retrieval_logits, lm_logits)
+
+    # assert
+    calls = [
+        _Call(((retrieval_logits,), {"dim": 1})),
+        _Call(((lm_logits,), {"dim": 1})),
+    ]
+    mock_torch_functional.softmax.assert_has_calls(calls)
+    mock_torch_functional.kl_div.assert_called_once()
+    assert out == torch.Tensor([expected])
