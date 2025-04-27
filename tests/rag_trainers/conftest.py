4bf5a08b25267b8775d99989388e6c59c9f165f5
nerdai <andrei@nerdai.io>
2025-04-27 19:32:08 -0400

    test init

--- tests/rag_trainers/conftest.py
+++ tests/rag_trainers/conftest.py
@@ -1,8 +1,10 @@
 from typing import Any
 
+import numpy as np
 import pytest
 import torch
 from pydantic import PrivateAttr
+from torch.utils.data import DataLoader, Dataset
 
 from fed_rag.base.fl_task import BaseFLTask
 from fed_rag.base.generator import BaseGenerator
@@ -10,7 +12,36 @@ from fed_rag.base.rag_trainer import BaseRAGTrainer
 from fed_rag.base.retriever import BaseRetriever
 from fed_rag.base.tokenizer import BaseTokenizer
 from fed_rag.knowledge_stores.in_memory import InMemoryKnowledgeStore
+from fed_rag.rag_trainers.pytorch import (
+    GeneratorTrainFn,
+    RetrieverTrainFn,
+    TrainingArgs,
+)
 from fed_rag.types.rag_system import RAGConfig, RAGSystem
+
+
+class _TestDataset(Dataset):
+    def __init__(self, size: int) -> None:
+        self.features = np.random.rand(size, 2)
+        self.labels = np.random.choice(2, size=size)
+
+    def __len__(self) -> int:
+        return len(self.labels)
+
+    def __getitem__(self, index: int) -> tuple[np.ndarray, Any]:
+        return self.features[index], self.labels[index]
+
+
+@pytest.fixture()
+def train_dataloader() -> DataLoader:
+    dataset = _TestDataset(size=10)
+    return DataLoader(dataset, batch_size=2, shuffle=True)
+
+
+@pytest.fixture()
+def val_dataloader() -> DataLoader:
+    dataset = _TestDataset(size=4)
+    return DataLoader(dataset, batch_size=2, shuffle=True)
 
 
 class MockRetriever(BaseRetriever):
@@ -118,3 +149,27 @@ class MockRAGTrainer(BaseRAGTrainer):
 
     def get_federated_task(self) -> BaseFLTask:
         raise NotImplementedError()
+
+
+@pytest.fixture()
+def retriever_trainer_fn() -> RetrieverTrainFn:
+    def fn(
+        rag_system: RAGSystem,
+        train_loader: DataLoader,
+        trainer_args: TrainingArgs,
+    ) -> Any:
+        return {"retriever_loss": 0.42}
+
+    return fn  # type: ignore
+
+
+@pytest.fixture()
+def generator_trainer_fn() -> GeneratorTrainFn:
+    def fn(
+        rag_system: RAGSystem,
+        train_loader: DataLoader,
+        trainer_args: TrainingArgs,
+    ) -> Any:
+        return {"generator_loss": 0.42}
+
+    return fn  # type: ignore
--- tests/rag_trainers/test_pytorch_rag_trainer.py
+++ tests/rag_trainers/test_pytorch_rag_trainer.py
@@ -1,7 +1,73 @@
+from torch.utils.data import DataLoader
+
 from fed_rag.base.rag_trainer import BaseRAGTrainer
-from fed_rag.rag_trainers.pytorch import PyTorchRAGTrainer
+from fed_rag.rag_trainers.pytorch import (
+    GeneratorTrainFn,
+    PyTorchRAGTrainer,
+    RetrieverTrainFn,
+    TrainingArgs,
+)
+from fed_rag.types.rag_system import RAGSystem
 
 
 def test_pt_rag_trainer_class() -> None:
     names_of_base_classes = [b.__name__ for b in PyTorchRAGTrainer.__mro__]
     assert BaseRAGTrainer.__name__ in names_of_base_classes
+
+
+def test_init(
+    mock_rag_system: RAGSystem,
+    retriever_trainer_fn: RetrieverTrainFn,
+    generator_trainer_fn: GeneratorTrainFn,
+    train_dataloader: DataLoader,
+) -> None:
+    retriever_trainer_args = TrainingArgs(
+        learning_rate=0.42, custom_kwargs={"param": True}
+    )
+    generator_trainer_args = TrainingArgs(
+        learning_rate=0.42, custom_kwargs={"param": False}
+    )
+
+    trainer = PyTorchRAGTrainer(
+        rag_system=mock_rag_system,
+        mode="retriever",
+        train_dataloader=train_dataloader,
+        retriever_training_args=retriever_trainer_args,
+        generator_training_args=generator_trainer_args,
+        retriever_train_fn=retriever_trainer_fn,
+        generator_train_fn=generator_trainer_fn,
+    )
+
+    assert trainer.rag_system == mock_rag_system
+    assert trainer.generator_train_fn == generator_trainer_fn
+    assert trainer.retriever_train_fn == retriever_trainer_fn
+    assert trainer.generator_training_args == generator_trainer_args
+    assert trainer.retriever_training_args == retriever_trainer_args
+    assert trainer.train_dataloader == train_dataloader
+
+
+def test_init_with_trainer_args_dict(
+    mock_rag_system: RAGSystem,
+    retriever_trainer_fn: RetrieverTrainFn,
+    generator_trainer_fn: GeneratorTrainFn,
+    train_dataloader: DataLoader,
+) -> None:
+    retriever_trainer_args = TrainingArgs(
+        learning_rate=0.42, custom_kwargs={"param": True}
+    )
+    generator_trainer_args = TrainingArgs(
+        learning_rate=0.42, custom_kwargs={"param": False}
+    )
+
+    trainer = PyTorchRAGTrainer(
+        rag_system=mock_rag_system,
+        mode="retriever",
+        train_dataloader=train_dataloader,
+        retriever_training_args=retriever_trainer_args.model_dump(),
+        generator_training_args=generator_trainer_args.model_dump(),
+        retriever_train_fn=retriever_trainer_fn,
+        generator_train_fn=generator_trainer_fn,
+    )
+
+    assert trainer.generator_training_args == generator_trainer_args
+    assert trainer.retriever_training_args == retriever_trainer_args
