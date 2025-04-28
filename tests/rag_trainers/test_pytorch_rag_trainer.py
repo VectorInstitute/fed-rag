27a4035cec691f000c56e7a262206117d067d0be
nerdai <andrei@nerdai.io>
2025-04-27 20:09:03 -0400

    similar unit tests for generator trainer

--- tests/rag_trainers/test_pytorch_rag_trainer.py
+++ tests/rag_trainers/test_pytorch_rag_trainer.py
@@ -4,7 +4,10 @@ import pytest
 from torch.utils.data import DataLoader
 
 from fed_rag.base.rag_trainer import BaseRAGTrainer
-from fed_rag.exceptions import UnspecifiedRetrieverTrainer
+from fed_rag.exceptions import (
+    UnspecifiedGeneratorTrainer,
+    UnspecifiedRetrieverTrainer,
+)
 from fed_rag.rag_trainers.pytorch import (
     GeneratorTrainFn,
     PyTorchRAGTrainer,
@@ -99,7 +102,6 @@ def test_init_with_no_trainer_args(
 def test_train_retriever(
     mock_prepare_retriever_for_training: MagicMock,
     mock_rag_system: RAGSystem,
-    generator_trainer_fn: GeneratorTrainFn,
     train_dataloader: DataLoader,
 ) -> None:
     mock_retriever_trainer_fn = MagicMock()
@@ -111,7 +113,6 @@ def test_train_retriever(
         mode="retriever",
         train_dataloader=train_dataloader,
         retriever_train_fn=mock_retriever_trainer_fn,
-        generator_train_fn=generator_trainer_fn,
         retriever_training_args=retriever_trainer_args,
     )
 
@@ -127,7 +128,6 @@ def test_train_retriever(
 def test_train_retriever_raises_unspecified_retriever_trainer_error(
     mock_prepare_retriever_for_training: MagicMock,
     mock_rag_system: RAGSystem,
-    generator_trainer_fn: GeneratorTrainFn,
     train_dataloader: DataLoader,
 ) -> None:
     retriever_trainer_args = TrainingArgs(
@@ -137,7 +137,6 @@ def test_train_retriever_raises_unspecified_retriever_trainer_error(
         rag_system=mock_rag_system,
         mode="retriever",
         train_dataloader=train_dataloader,
-        generator_train_fn=generator_trainer_fn,
         retriever_training_args=retriever_trainer_args,
     )
 
@@ -147,3 +146,53 @@ def test_train_retriever_raises_unspecified_retriever_trainer_error(
     ):
         trainer.train()
         mock_prepare_retriever_for_training.assert_called_once()
+
+
+@patch.object(PyTorchRAGTrainer, "_prepare_generator_for_training")
+def test_train_generator(
+    mock_prepare_generator_for_training: MagicMock,
+    mock_rag_system: RAGSystem,
+    train_dataloader: DataLoader,
+) -> None:
+    mock_generator_trainer_fn = MagicMock()
+    generator_trainer_args = TrainingArgs(
+        learning_rate=0.42, custom_kwargs={"param": True}
+    )
+    trainer = PyTorchRAGTrainer(
+        rag_system=mock_rag_system,
+        mode="generator",
+        train_dataloader=train_dataloader,
+        generator_train_fn=mock_generator_trainer_fn,
+        generator_training_args=generator_trainer_args,
+    )
+
+    trainer.train()
+
+    mock_prepare_generator_for_training.assert_called_once()
+    mock_generator_trainer_fn.assert_called_once_with(
+        mock_rag_system, train_dataloader, generator_trainer_args
+    )
+
+
+@patch.object(PyTorchRAGTrainer, "_prepare_generator_for_training")
+def test_train_generator_raises_unspecified_generator_trainer_error(
+    mock_prepare_generator_for_training: MagicMock,
+    mock_rag_system: RAGSystem,
+    train_dataloader: DataLoader,
+) -> None:
+    generator_trainer_args = TrainingArgs(
+        learning_rate=0.42, custom_kwargs={"param": True}
+    )
+    trainer = PyTorchRAGTrainer(
+        rag_system=mock_rag_system,
+        mode="generator",
+        train_dataloader=train_dataloader,
+        generator_training_args=generator_trainer_args,
+    )
+
+    with pytest.raises(
+        UnspecifiedGeneratorTrainer,
+        match="Attempted to perform generator trainer with an unspecified trainer function.",
+    ):
+        trainer.train()
+        mock_prepare_generator_for_training.assert_called_once()
