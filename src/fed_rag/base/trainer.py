1da0508b37124ada67a294bea8eb0cc01addd259
nerdai <andrei@nerdai.io>
2025-04-29 10:49:07 -0400

    BaseTrainer

---
+++ src/fed_rag/base/trainer.py
@@ -0,0 +1,23 @@
+"""Base Trainer"""
+
+from abc import ABC, abstractmethod
+
+from pydantic import BaseModel, ConfigDict
+
+from fed_rag.types.rag_system import RAGSystem
+from fed_rag.types.results import TestResult, TrainResult
+
+
+class BaseTrainer(BaseModel, ABC):
+    """Base Trainer Class."""
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    rag_system: RAGSystem
+
+    @abstractmethod
+    def train(self) -> TrainResult:
+        """Train loop."""
+
+    @abstractmethod
+    def evaluate(self) -> TestResult:
+        """Evaluation"""
