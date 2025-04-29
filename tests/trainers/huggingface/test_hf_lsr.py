aa2dd1bbfaabe00ec5943002d5190992a57beb46
nerdai <andrei@nerdai.io>
2025-04-29 16:06:33 -0400

    test raises trainer error

--- tests/trainers/huggingface/test_hf_lsr.py
+++ tests/trainers/huggingface/test_hf_lsr.py
@@ -1,6 +1,8 @@
+import pytest
 from datasets import Dataset
 from pytest import MonkeyPatch
 
+from fed_rag.exceptions import TrainerError
 from fed_rag.trainers.huggingface.lsr import HuggingFaceLSRTrainer
 from fed_rag.types.rag_system import RAGSystem
 
@@ -20,3 +22,25 @@ def test_init(
     assert trainer.train_dataset == train_dataset
     assert trainer.model == hf_rag_system.retriever.encoder
     assert trainer.rag_system == hf_rag_system
+
+
+def test_invalid_retriever_raises_error(
+    mock_rag_system: RAGSystem,
+    train_dataset: Dataset,
+    monkeypatch: MonkeyPatch,
+) -> None:
+    # skip validation of rag system
+    monkeypatch.setenv("FEDRAG_SKIP_VALIDATION", "1")
+
+    with pytest.raises(
+        TrainerError,
+        match=(
+            "For `HuggingFaceLSRTrainer`, attribute `model` must be of type "
+            "`~sentence_transformers.SentenceTransformer`."
+        ),
+    ):
+        HuggingFaceLSRTrainer(
+            model=mock_rag_system.retriever.encoder,
+            rag_system=mock_rag_system,
+            train_dataset=train_dataset,
+        )
