575b9830b8cdaa2cb13667f94ec33874c4b7bc78
nerdai <andrei@nerdai.io>
2025-05-12 15:35:37 -0400

    return FedRAGManagedMixin

--- src/fed_rag/bridges/llamaindex/bridge.py
+++ src/fed_rag/bridges/llamaindex/bridge.py
@@ -19,6 +19,10 @@ class LlamaIndexBridgeMixin(BaseBridgeMixin):
     _framework = "llama-index-core"
     _compatible_versions = ["0.12.35"]
 
-    def to_llamaindex(self: "RAGSystem") -> BaseManagedIndex:
+    def to_llamaindex(self: "RAGSystem") -> "BaseManagedIndex":
         """Converts the RAGSystem to a ~llamaindex.core.BaseManagedIndex."""
-        pass
+        from fed_rag.bridges.llamaindex._managed_index import (
+            FedRAGManagedIndex,
+        )
+
+        return FedRAGManagedIndex(rag_system=self)
