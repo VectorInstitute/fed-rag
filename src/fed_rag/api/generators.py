e05e9e03a5c061eeb114ae94ee8edf747aa9b918
nerdai <andrei@nerdai.io>
2025-05-17 08:53:56 -0400

    rm public api in favor of root imports

--- src/fed_rag/api/generators.py
+++
@@ -1,8 +0,0 @@
-"""Generator classes in the public API."""
-
-from fed_rag.generators.huggingface import (
-    HFPeftModelGenerator,
-    HFPretrainedModelGenerator,
-)
-
-__all__ = ["HFPeftModelGenerator", "HFPretrainedModelGenerator"]
--- src/fed_rag/api/retrievers.py
+++
@@ -1,5 +0,0 @@
-"""Generator classes in the public API."""
-
-from fed_rag.retrievers.huggingface import HFSentenceTransformerRetriever
-
-__all__ = ["HFSentenceTransformerRetriever"]
