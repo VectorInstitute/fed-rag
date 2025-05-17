361453c9a428e3a5b0fb904dc65ae9104972ef3c
nerdai <andrei@nerdai.io>
2025-05-16 23:34:48 -0400

    add generators to public api

--- src/fed_rag/api/__init__.py
+++ src/fed_rag/api/__init__.py
@@ -10,12 +10,14 @@ For most use cases, this is the only module you need to import from: fed_rag.api
 # ruff: noqa: F403, F401
 from .core import *
 from .core import __all__ as _core_all
+from .generators import *
+from .generators import __all__ as _generators_all
 from .knowledge_stores import *
 from .knowledge_stores import __all__ as _knowledge_stores_all
 from .types import *
 from .types import __all__ as _types_all
 
-API_VERSION = "0.0.1"
+API_VERSION = "0.0.2"
 
 
 def get_api_version() -> str:
@@ -25,6 +27,7 @@ def get_api_version() -> str:
 
 __all__ = sorted(
     _core_all
+    + _generators_all
     + _knowledge_stores_all
     + _types_all
     + ["API_VERSION", "get_api_version"]
