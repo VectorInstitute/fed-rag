2bdba13ec13578da393e7dbabfa7e06b2aa98819
nerdai <andrei@nerdai.io>
2025-05-19 13:35:28 -0400

    implement abstract field extractors for mmlu

--- src/fed_rag/evals/benchmarks/huggingface/mmlu.py
+++ src/fed_rag/evals/benchmarks/huggingface/mmlu.py
@@ -9,32 +9,36 @@ from fed_rag.base.evals.benchmark import BaseBenchmark
 from .mixin import HuggingFaceBenchmarkMixin
 from .utils import check_huggingface_evals_installed
 
-# {
-#     "question": "What is the embryological origin of the hyoid bone?",
-#     "choices": [
-#         "The first pharyngeal arch",
-#         "The first and second pharyngeal arches",
-#         "The second pharyngeal arch",
-#         "The second and third pharyngeal arches",
-#     ],
-#     "answer": "D",
-# }
-
 
 class HuggingFaceMMLU(HuggingFaceBenchmarkMixin, BaseBenchmark):
-    """HuggingFace MMLU Benchmark."""
+    """HuggingFace MMLU Benchmark.
+
+    Example schema:
+        {
+            "question": "What is the embryological origin of the hyoid bone?",
+            "choices": [
+                "The first pharyngeal arch",
+                "The first and second pharyngeal arches",
+                "The second pharyngeal arch",
+                "The second and third pharyngeal arches",
+            ],
+            "answer": "D",
+        }
+    """
 
     dataset_name = "cais/mmlu"
     configuration_name: str = "all"
 
     def _get_query_from_example(self, example: dict[str, Any]) -> str:
-        raise NotImplementedError
+        choices = example["choices"]
+        formatted_choices = f"A: {choices[0]}\nB: {choices[1]}\nC: {choices[2]}\nD: {choices[3]}"
+        return f"{example['question']}\n\n{formatted_choices}"
 
     def _get_response_from_example(self, example: dict[str, Any]) -> str:
-        raise NotImplementedError
+        return str(example["answer"])
 
-    def _get_context_from_example(self, example: dict[str, Any]) -> str:
-        raise NotImplementedError
+    def _get_context_from_example(self, example: dict[str, Any]) -> str | None:
+        return None
 
     @model_validator(mode="before")
     @classmethod
