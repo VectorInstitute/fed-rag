6e329d6734b33f55b5fbad0c9590d919f379410d
nerdai <andrei@nerdai.io>
2025-05-18 21:08:14 -0400

    unit test for base benchmark

--- src/fed_rag/base/evals/benchmark.py
+++ src/fed_rag/base/evals/benchmark.py
@@ -1,9 +1,9 @@
 """Base Benchmark and Benchmarker"""
 
 from abc import ABC, abstractmethod
-from typing import Any, Sequence
+from typing import Any, Iterator, Sequence
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, PrivateAttr, model_validator
 
 from fed_rag import RAGSystem
 from fed_rag.data_structures.evals import BenchmarkExample, BenchmarkResult
@@ -12,10 +12,29 @@ from fed_rag.data_structures.evals import BenchmarkExample, BenchmarkResult
 class BaseBenchmark(BaseModel, ABC):
     """Base Data Collator."""
 
-    name: str
-    examples: Sequence[BenchmarkExample]
+    _examples: Sequence[BenchmarkExample] = PrivateAttr()
 
     model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    @abstractmethod
+    def _get_examples(self, **kwargs: Any) -> Sequence[BenchmarkExample]:
+        """Method to get examples."""
+
+    # give it a sequence interface for accessing examples more easily
+    def __getitem__(self, index: int) -> BenchmarkExample:
+        return self._examples.__getitem__(index)
+
+    def __len__(self) -> int:
+        return self._examples.__len__()
+
+    # shouldn't override Pydantic BaseModels' __iter__
+    def as_iterator(self) -> Iterator[BenchmarkExample]:
+        return self._examples.__iter__()
+
+    @model_validator(mode="after")
+    def set_examples(self) -> "BaseBenchmark":
+        self._examples = self._get_examples()
+        return self
 
 
 class BaseBenchmarker(BaseModel, ABC):
