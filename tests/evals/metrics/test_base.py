675dff47d5ecb460156e1ea2cd89193120519ebd
nerdai <andrei@nerdai.io>
2025-05-18 23:20:22 -0400

    test metric

--- tests/evals/metrics/test_base.py
+++ tests/evals/metrics/test_base.py
@@ -8,3 +8,11 @@ class MyMetric(BaseEvaluationMetric):
         self, prediction: str, actual: str, *args: Any, **kwargs: Any
     ) -> float:
         return 0.42
+
+
+def test_metric_call() -> None:
+    metric = MyMetric()
+
+    score = metric("fake pred", "fake actual")
+
+    assert score == 0.42
