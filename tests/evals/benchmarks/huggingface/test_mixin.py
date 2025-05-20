0413b224d24c70fabf5548486196bf9145fe4655
nerdai <andrei@nerdai.io>
2025-05-20 01:02:15 -0400

    handle streaming

--- tests/evals/benchmarks/huggingface/test_mixin.py
+++ tests/evals/benchmarks/huggingface/test_mixin.py
@@ -20,3 +20,14 @@ def test_hf_mixin(
         == test_hf_benchmark._dataset.info.dataset_name
     )
     assert isinstance(test_hf_benchmark[0], BenchmarkExample)
+
+
+@patch("datasets.load_dataset")
+def test_hf_streaming(
+    mock_load_dataset: MagicMock, dummy_dataset: Dataset
+) -> None:
+    mock_load_dataset.return_value = dummy_dataset
+    test_hf_benchmark = benchmarks.TestHFBenchmark(streaming=True)
+
+    example_stream = test_hf_benchmark.as_stream()
+    print(next(example_stream).model_dump())
