8952bf29a17edb8eed507ad4b17499cfb110a5b5
nerdai <andrei@nerdai.io>
2025-05-21 15:21:42 -0400

    knowledge node embedding as optional; qdrant doesn't include embedding in payload

--- src/fed_rag/knowledge_stores/qdrant/sync.py
+++ src/fed_rag/knowledge_stores/qdrant/sync.py
@@ -59,17 +59,29 @@ def _convert_knowledge_node_to_qdrant_point(
 ) -> "PointStruct":
     from qdrant_client.models import PointStruct
 
+    if node.embedding is None:
+        warnings.warn(
+            "Uploading a node that has embedding set to None.",
+            KnowledgeStoreWarning,
+        )
+
     return PointStruct(
-        id=node.node_id, vector=node.embedding, payload=node.model_dump()
+        id=node.node_id,
+        vector=node.embedding,
+        payload=node.model_dump_without_embeddings(),
     )
 
 
 def _convert_scored_point_to_knowledge_node_and_score_tuple(
     scored_point: "ScoredPoint",
 ) -> tuple[float, KnowledgeNode]:
+    knowledge_data = scored_point.payload
+    knowledge_data.update(
+        embedding=scored_point.vector
+    )  # attach vector to embedding if it is even returned
     return (
         scored_point.score,
-        KnowledgeNode.model_validate(scored_point.payload),
+        KnowledgeNode.model_validate(knowledge_data),
     )
 
 
