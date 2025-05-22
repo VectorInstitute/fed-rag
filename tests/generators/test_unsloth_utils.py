3ccad467de913384959031b6339f873121bbde29
nerdai <andrei@nerdai.io>
2025-05-22 01:19:48 -0400

    start unit tests

---
+++ tests/generators/test_unsloth_utils.py
@@ -0,0 +1,24 @@
+import re
+from unittest.mock import patch
+
+import pytest
+
+from fed_rag.exceptions import MissingExtraError
+from fed_rag.generators.unsloth.utils import check_unsloth_installed
+
+
+def test_check_raises_error() -> None:
+    """Check raises error from utils."""
+
+    modules = {"unsloth": None}
+
+    with patch.dict("sys.modules", modules):
+        msg = (
+            "Missing installation of the `unsloth` extra, yet is required "
+            "by an imported class. To fix please run `pip install fed-rag[unsloth]`."
+        )
+        with pytest.raises(
+            MissingExtraError,
+            match=re.escape(msg),
+        ):
+            check_unsloth_installed()
