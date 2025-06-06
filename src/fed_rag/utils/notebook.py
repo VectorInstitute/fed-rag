b18c45f0390db660f8f41561f67c835a1fc895ef
nerdai <andrei@nerdai.io>
2025-06-05 21:57:23 -0400

    init

---
+++ src/fed_rag/utils/notebook.py
@@ -0,0 +1,9 @@
+"""Utils for notebooks"""
+
+
+class ProcessMonitor:
+    """A class for launching, managing and monitoring subprocesses within Jupyter Notebooks.
+
+    NOTE: This is intended mainly for launching and managing FL servers and
+    client processes.
+    """
