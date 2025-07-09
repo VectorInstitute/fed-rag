53b94a131caab8661105daf2cc7205af882b1cfe
CalculusC <chiho00hk@hotmail.com>
2025-07-09 01:26:00 -0400

    add test

--- src/fed_rag/base/retriever_mixins/__init__.py
+++ src/fed_rag/base/retriever_mixins/__init__.py
@@ -1,3 +1,12 @@
+from .audio import AudioRetrieverMixin, RetrieverHasAudioModality
 from .image import ImageRetrieverMixin, RetrieverHasImageModality
+from .video import RetrieverHasVideoModality, VideoRetrieverMixin
 
-__all__ = ["ImageRetrieverMixin", "RetrieverHasImageModality"]
+__all__ = [
+    "ImageRetrieverMixin",
+    "RetrieverHasImageModality",
+    "AudioRetrieverMixin",
+    "RetrieverHasAudioModality",
+    "VideoRetrieverMixin",
+    "RetrieverHasVideoModality",
+]
